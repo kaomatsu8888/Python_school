b9686934f7dad1b98b5e4d8a95a4b221da486bde
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2024-02-22 14:40:44 +0900

    github学習2

--- 1.24PythonFirst_grader(1day)/bmi.py
+++ 1.24PythonFirst_grader(1day)/bmi.py
@@ -1,6 +1,6 @@
 h = float(input("身長何cmですか？")) / 100.0
 w = float(input("体重何kgですか？"))
-bmi = w / (h * h)
+bmi = w / (h * h) #BMI値を計算
 print(f"あなたのBMI値は、{bmi}です")#f記法自分で勉強したやつ楽だよね
 print("あなたのBMI値は、",bmi," です。")#これが授業で習ったやつ
 print("あなたのBMI値は、bmiです。")#これは表示されないよ
